945f93a0a32563f1d8f949b9c928cbb178a6f041
I03D <98839825+I03D@users.noreply.github.com>
2023-12-12 22:04:14 +0700

    labeled.

--- nnet_28.11/main.py
+++ nnet_28.11/main.py
@@ -1,29 +1,40 @@
+# Максименко И-03
 import numpy as np
 import scipy
 from scipy import special
+# Максименко И-03
 
 I = np.array([0.9, 0.1, 0.8], ndmin=2).T
 t = np.array([0.1, 0.4, 0.9], ndmin=2).T
+# Максименко И-03
 
 W_i_h = np.array([[0.9, 0.3, 0.4],
                   [0.2, 0.8, 0.2],
                   [0.1, 0.5, 0.6]])
 
 W_h_o = np.array([[0.3, 0.7, 0.5],
+# Максименко И-03
                   [0.6, 0.5, 0.2],
                   [0.8, 0.1, 0.9]])
 
+# Максименко И-03
 for i in range(1000):
     X_h = W_i_h.dot(I)
     O_h = scipy.special.expit(X_h)
+# Максименко И-03
+    
     X_o = W_h_o.dot(O_h)
     O = scipy.special.expit(X_o)
+# Максименко И-03
 
     E_o = t - O
     E_h = np.dot(W_h_o.T, E_o)
+# Максименко И-03
 
     W_h_o += 0.2 * E_o * O * (1 - O) * O_h.T
     W_i_h += 0.2 * E_h * O_h * (1 - O_h) * I.T
+# Максименко И-03
 
 print(O)
 
+# Максименко И-03
